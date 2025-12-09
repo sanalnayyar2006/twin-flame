c33716ba319f5a5932913bf282759dbeedde010a
sanalnayyar <sanalsquad@gmail.com>
2025-12-09 16:07:55 +0530

    fixing

--- backend/src/routes/truthDare.js
+++ backend/src/routes/truthDare.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
 import Question from "../models/Question.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
